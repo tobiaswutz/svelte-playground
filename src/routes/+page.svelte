bb1fd6e42b386849c3d7b29901fb6a4cccee8be1
Tobias <tobiaswutz22@gmail.com>
2023-01-16 02:18:17 +0100

    new components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
 	let active: boolean = false;
+	import Button from "$lib/components/Button.svelte";
+	import Dropmenu from "$lib/components/Dropmenu.svelte";
+
 
 	const toggleActieve = () => {
 		active = !active;
+		console.log('Hallo mein Name ist Tobias ich habe gerade diesesn Butten gehovert!')
 	};
 </script>
 
 <div class="h-screen text-white bg-gray-900">
 	<div>
 		<div class="p-10 h-screen w-full">
-			<div class="h-full w-full border-2 border-orange-400 flex justify-center items-center">
-				<button
-					on:click={toggleActieve}
-					class="p-2 rounded-lg border-2 border-green-400"
-					class:active={active}>ICH BIN EIN BUTTON!</button
-				>
-			</div>
+				<Dropmenu />
+
+			<!-- <div class="h-full w-full border-2 border-orange-400 flex justify-center items-center">
+				<Button on:click={toggleActieve}>Hallo ich</Button>
+				<Dropmenu />
+			</div> -->
 		</div>
 	</div>
 </div>
